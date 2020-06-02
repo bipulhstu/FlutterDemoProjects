6c595e72731c9344fdc355787ff436b334d8d1cb
jamal-pb95 <jamal.pb95@gmail.com>
2020-06-02 15:50:52 +0600

    add projects

---
+++ muslim_checklist/android/app/src/main/kotlin/jaamaal/com/muslim_checklist/MainActivity.kt
@@ -0,0 +1,12 @@
+package jaamaal.com.muslim_checklist
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
